56507ae3ade77cc9485792ae82a8bfef85951063
Glazoff <glazov_evgen@mail.ru>
2023-10-27 20:55:08 +0300

    sprint_1/feat/add_script_start

--- messenger/vite.config.js
+++ messenger/vite.config.js
@@ -3,14 +3,24 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
-	build: {
-		outDir: resolve(__dirname, 'dist'),
-	},
-  plugins: [handlebars({
+  	root: resolve(__dirname, 'src'),
+  	plugins: [handlebars({
 		partialDirectory: resolve(__dirname, 'src/partials'),
 		context: {
 			titleApp: 'My messenger'
 		},
 	})],
+
+	build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'src/index.html'),
+                auth: resolve(__dirname, 'src/pages/auth/auth.html'),
+                registration: resolve(__dirname, 'src/pages/registration/registration.html'),
+                profile: resolve(__dirname, 'src/pages/profile/profile.html'),
+                404: resolve(__dirname, 'src/pages/404/404.html'),
+				500: resolve(__dirname, 'src/pages/500/500.html'),
+            },
+        },
+    },
 })
