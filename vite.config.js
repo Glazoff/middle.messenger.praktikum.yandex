d9f7ffa8a3a2491cd5a34d274f1c2dfe25b171a9
Glazoff <glazov_evgen@mail.ru>
2023-10-28 13:25:03 +0300

    sprint_1/fix_remove_files

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
 	})],
 
 	build: {
+        outDir: resolve(__dirname, 'dist'),
         rollupOptions: {
             input: {
                 index: resolve(__dirname, 'src/index.html'),
