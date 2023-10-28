c1995531d3c69107e0ef333e09fc4d5177260031
Glazoff <glazov_evgen@mail.ru>
2023-10-28 14:23:47 +0300

    add_fix_port

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,12 @@ export default defineConfig({
 			titleApp: 'My messenger'
 		},
 	})],
-
+    server: {
+        port: 3000,
+    },
+    preview: {
+        port: 3000,
+    },
 	build: {
         outDir: resolve(__dirname, 'dist'),
         rollupOptions: {
