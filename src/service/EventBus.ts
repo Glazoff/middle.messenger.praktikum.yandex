8cfc9863e1835deb5d992cc22f55aca6aaa0dbdd
Glazoff <glazov_evgen@mail.ru>
2023-11-12 15:35:40 +0300

    sprint_1/feat/add/EventBus

---
+++ src/service/EventBus.ts
@@ -0,0 +1,37 @@
+export class EventBus {
+    //todo фиксить тип после добавления конфига
+    listeners: {}
+
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event, callback) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+
+        this.listeners[event].push(callback);
+    }
+
+    off(event, callback) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+
+        this.listeners[event] = this.listeners[event].filter(
+            (listener) => listener !== callback
+        );
+    }
+
+    emit(event, ...args) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+
+        this.listeners[event].forEach((listener) => {
+            listener(...args);
+        });
+    }
+}
+  
