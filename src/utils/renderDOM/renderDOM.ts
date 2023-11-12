01b112133acb919fa1a3e94ea725e0824bd8c99f
Glazoff <glazov_evgen@mail.ru>
2023-11-12 16:58:33 +0300

    sprint_1/feat/add_renderDOM

---
+++ src/utils/renderDOM/renderDOM.ts
@@ -0,0 +1,16 @@
+
+
+export function renderDOM(query: string, component: any) { // todo присвоить тип component: класс component 
+    const root = document.querySelector(query);
+
+    if(!root) {
+        throw Error(`Елемент ${query} не найден`)
+    }
+  
+    root.appendChild(component.getContent());
+  
+    component.dispatchComponentDidMount();
+  
+    return root;
+}
+  
