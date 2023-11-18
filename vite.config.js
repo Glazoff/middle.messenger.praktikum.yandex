6800304172bc381f4f43e3b4d21e9ed0490a2931
Glazoff <glazov_evgen@mail.ru>
2023-11-18 15:05:00 +0300

    add_page_main_config

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'src/index.html'),
+        main: resolve(__dirname, 'src/pages/main'),
         auth: resolve(__dirname, 'src/pages/auth'),
         registration: resolve(__dirname, 'src/pages/registration'),
         profile: resolve(__dirname, 'src/pages/profile'),
